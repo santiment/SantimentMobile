66b50091a2b3929e435d8f92a56bc826ba84a3f4
Eugene Tartakovsky <flar49@gmail.com>
2017-03-27 21:19:50 +0300

    Add Santiment API client mockup

--- app/api/santiment/index.js
+++ app/api/santiment/index.js
@@ -13,17 +13,17 @@ import axios from 'axios'
 
 // add types
 export const getSentiment = () => {
-    return Rx.Observable.of(
-        {
+    return Rx.Observable.timer(1000).first().map(() => {
+        return [{
             "userId": "TESTUSER",
-            "sentiment": [
-                {"id": "aaa", "asset": "BTC", "sentiment": "bullish", "date": "2017-03-16T23:23:41.229Z"},
-                {"id": "bbb", "asset": "BTC", "sentiment": "bullish", "date": "2017-03-17T23:23:41.229Z"},
-                {"id": "aAA", "asset": "ETH", "sentiment": "catish", "date": "2017-03-14T23:23:41.229Z"},
-                {"id": "EEE", "asset": "ETH", "sentiment": "bearish", "date": "2017-03-15T23:23:41.229Z"},
-                {"id": "GGG", "asset": "ETH", "sentiment": "catish", "date": "2017-03-16T23:23:41.229Z"},
-                {"id": "ZZZ", "asset": "ETH", "sentiment": "bearish", "date": "2017-03-17T23:23:41.229Z"}
+            "data": [
+                {"id": "aaa", "symbol": "BTC/USD", "sentiment": "bullish", "price": 1041, "date": "2017-03-16T23:23:41.229Z"},
+                {"id": "bbb", "symbol": "BTC/USD", "sentiment": "bullish", "price": 1043,  "date": "2017-03-17T23:23:41.229Z"},
+                {"id": "aAA", "symbol": "ETH/USD", "sentiment": "catish", "price": 1046, "date": "2017-03-14T23:23:41.229Z"},
+                {"id": "EEE", "symbol": "ETH/USD", "sentiment": "bearish", "price": 1049, "date": "2017-03-15T23:23:41.229Z"},
+                {"id": "GGG", "symbol": "BTC/USD", "sentiment": "catish", "price": 1053, "date": "2017-03-16T23:23:41.229Z"},
+                {"id": "ZZZ", "symbol": "ETH/USD", "sentiment": "bearish", "price": 1011, "date": "2017-03-17T23:23:41.229Z"}
             ]
-        }
-    )
+        }];
+    })
 };
