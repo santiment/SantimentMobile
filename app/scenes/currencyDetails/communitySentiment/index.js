d2ea69c2df3b1eeb6bc4e7580d57bdd5e5ec447b
Eugene Tartakovsky <flar49@gmail.com>
2017-04-20 23:54:56 +0300

    Implement CommunitySentiment screen draft

--- app/scenes/currencyDetails/communitySentiment/index.js
+++ app/scenes/currencyDetails/communitySentiment/index.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import ReactNative from 'react-native';
-let {Text, View} = ReactNative;
+let {Text, View, StyleSheet, Image} = ReactNative;
 
 import {observer} from 'mobx-react/native'
 
@@ -17,10 +17,45 @@ export default class CommunitySentiment extends React.Component {
         const {navigator, store} = this.props;
 
         return (
-            <View>
-                <Text>{`Bullish: ${store.aggregate.bullish}`}</Text>
-                <Text>{`Catish: ${store.aggregate.catish}`}</Text>
-                <Text>{`Bearish: ${store.aggregate.bearish}`}</Text>
+            <View style={styles.container}>
+                <View style={styles.column}>
+                    <Text style={styles.text}>
+                        {`${store.aggregate.bullish}`}
+                    </Text>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            style={styles.image}
+                            source={require('../../../resources/images/bull.png')}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.column}>
+                    <Text style={styles.text}>
+                        {`${store.aggregate.catish}`}
+                    </Text>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            style={styles.image}
+                            source={require('../../../resources/images/cat.png')}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.column}>
+                    <Text style={styles.text}>
+                        {`${store.aggregate.bearish}`}
+                    </Text>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            style={styles.image}
+                            source={require('../../../resources/images/bear.png')}
+                        />
+                    </View>
+                </View>
+
+
+
             </View>
         )
     }
@@ -35,3 +70,31 @@ CommunitySentiment.propTypes = {
         aggregate: React.PropTypes.any.isRequired
     })
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    column: {
+        width: 80,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        padding: 5,
+        height: 60,
+        width: 60
+    },
+    image: {
+        height: 50,
+        width: 50,
+    },
+    text: {
+        fontSize: 36,
+        fontWeight: "500",
+    },
+});
