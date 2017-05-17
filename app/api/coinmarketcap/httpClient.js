65700938076a87f2ec250091afc5a557164c036c
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-05-17 22:26:25 +0300

    Implemented HTTP client for CoinMarketCap API

---
+++ app/api/coinmarketcap/httpClient.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import Rx from 'rxjs';
+import axios from 'axios';
+import moment from 'moment';
+import _ from 'lodash';
+
+/**
+ * Base URL for API endpoints.
+ */
+const apiUrl = "https://api.coinmarketcap.com/v1";
+
+/**
+ * Makes asynchronous request for ticker.
+ * @param {number} limit Limit.
+ * @return Axios request.
+ */
+export const getTicker = (limit: number) => {
+    /**
+     * Obtain URL for request.
+     */
+    const url = apiUrl + `/ticker?limit=${limit}`;
+
+    /**
+     * Start request and return it.
+     */
+    return axios.get(url);
+};
