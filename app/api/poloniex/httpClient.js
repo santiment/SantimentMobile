5b62d78c15696ebd3a478271bf490f7268a77860
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-05-18 19:44:06 +0300

    Updated documentation for Poloniex HTTP client

--- app/api/poloniex/httpClient.js
+++ app/api/poloniex/httpClient.js
@@ -30,10 +30,13 @@ export const getTickers = (): any => {
 /**
  * Makes asynchronous request for candles.
  * 
- * @param {string} symbol Currency pair associated with candles.
- * @param {Date} from Start date.
- * @param {Date} to End date.
- * @param {number} period Period.
+ * Example of API request:
+ * https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=9999999999&period=14400
+ * 
+ * @param {string} symbol Currency pair, e.g. "BTC_STEEM".
+ * @param {Date} startDate Start date.
+ * @param {Date} endDate End date.
+ * @param {number} period Period in seconds, e.g. 14400.
  * @return Axios request.
  */
 export const getCandles = (symbol: string, startDate: Date, endDate: Date, period: number): any => {
