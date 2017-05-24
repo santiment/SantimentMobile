35bddbad666ad9a96839d070d7e565c80ba46b28
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-05-25 01:58:29 +0300

    Update list view on MySentiment scene

--- app/scenes/currencyDetails/mySentiment/index.js
+++ app/scenes/currencyDetails/mySentiment/index.js
@@ -269,8 +269,6 @@ const styles = StyleSheet.create({
     },
     listViewContainer: {
         backgroundColor: 'white',
-        paddingLeft: 20,
-        paddingRight: 20,
         flex: 1,
     },
     tabsContainerStyle: {
