ae72e87ceabb168da9ac37cd4c0080acf84b6fd0
Eugene Tartakovsky <flar49@gmail.com>
2017-05-30 17:31:21 +0300

    eslint: fix Currencies scene

--- app/scenes/currencies/index.js
+++ app/scenes/currencies/index.js
@@ -3,21 +3,20 @@
  * @flow
  */
 
-'use strict';
 
 import React from 'react';
 
-import ReactNative, {
+import {
     View,
     Text,
     ListView,
     RefreshControl,
-    StyleSheet
+    StyleSheet,
 } from 'react-native';
 
 import {
     Icon,
-    Button
+    Button,
 } from 'react-native-elements';
 
 import NavigationBar from 'react-native-navbar';
@@ -26,140 +25,27 @@ import _ from 'lodash';
 
 import moment from 'moment';
 
-import {observer} from 'mobx-react/native';
+import { observer } from 'mobx-react/native';
 
 
 import {
     AddCurrencyRoute,
     CurrencyDetailsRoute,
-    EditCurrenciesRoute
+    EditCurrenciesRoute,
 } from '../../navigator/routes';
 
 import Cell from './cell';
 
-
-@observer
-export default class Currencies extends React.Component {
-    render() {
-        const {navigator, store} = this.props;
-
-        const renderRow = (data, sectionID, rowID) => {
-            return (
-                <Cell
-                    symbol={data.displaySymbol}
-                    price={data.price}
-                    dailyChangePercent={data.dailyChangePercent}
-                    onPress={ () => {
-                        store.selectSymbol(data.symbol);
-                        navigator.push({name: CurrencyDetailsRoute})
-                    }}
-                    onVote={(sentiment) => {
-                        store.selectSymbol(data.symbol);
-                        store.addSentiment({
-                            asset: data.symbol,
-                            price: data.price,
-                            sentiment: sentiment,
-                            timestamp: moment().unix(),
-                        });
-                        navigator.push({name: CurrencyDetailsRoute})
-                    }}
-                />
-
-            )
-        };
-
-        const renderSeparator = (sectionID, rowID, adjacentRowHighlighted) => {
-            return (<View key={rowID} style={styles.separator}/>)
-        };
-
-        const addCurrency = () => {
-            navigator.push({name: AddCurrencyRoute})
-        };
-
-        const isEmpty = _.isEmpty(store.tickers.slice());
-
-        const noDataView = (
-            <View style={styles.noData}>
-                <Text style={styles.noDataText}>Nothing here.</Text>
-                <Button
-                    raised
-                    buttonStyle={styles.noDataButton}
-                    textStyle={{textAlign: 'center'}}
-                    title={`Add coins`}
-                    backgroundColor={"#27aa36"}
-                    fontSize={20}
-                    onPress={addCurrency}
-                />
-            </View>
-        );
-
-        const listView = (
-            <ListView
-                style={styles.listView}
-                renderRow={renderRow}
-                renderHeader={() => <View style={styles.header}/>}
-                renderFooter={() => <View style={styles.footer}/>}
-                renderSeparator={renderSeparator}
-                dataSource={store.dataSource}
-                enableEmptySections={true}
-                removeClippedSubviews={false}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={store.isLoading}
-                        onRefresh={store.refresh}
-                    />
-                }
-            />
-        );
-
-        const contentView = isEmpty ? noDataView : listView;
-
-        return (
-            <View style={styles.container}>
-
-                <NavigationBar
-                    title={{title: "Currencies"}}
-                    style={styles.navBar}
-                    rightButton={
-                        <Icon
-                            containerStyle={styles.toolbarButton}
-                            name="mode-edit"
-                            onPress={ () => {
-                                navigator.push({name: EditCurrenciesRoute})
-                            }}
-                        />
-                    }
-                />
-
-                {contentView}
-
-                <View style={styles.fabContainer}>
-
-                    <Icon
-                        raised
-                        reverse
-                        name="add"
-                        color="#27aa36"
-                        onPress={addCurrency}
-                    />
-
-                </View>
-
-            </View>
-        );
-    }
-}
-
-Currencies.propTypes = {
+const propTypes = {
     navigator: React.PropTypes.shape({
         push: React.PropTypes.func.isRequired,
-        pop: React.PropTypes.func.isRequired
-    }),
+        pop: React.PropTypes.func.isRequired,
+    }).isRequired,
     store: React.PropTypes.shape({
         refresh: React.PropTypes.func.isRequired,
         isLoading: React.PropTypes.any.isRequired,
-        dataSource: React.PropTypes.any.isRequired
-    })
+        dataSource: React.PropTypes.any.isRequired,
+    }).isRequired,
 };
 
 const styles = StyleSheet.create({
@@ -168,7 +54,7 @@ const styles = StyleSheet.create({
     },
     navBar: {
         borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: "#cccccc",
+        borderBottomColor: '#cccccc',
     },
     listView: {
         backgroundColor: '#f0f0f0',
@@ -204,12 +90,127 @@ const styles = StyleSheet.create({
     },
     noDataText: {
         fontSize: 32,
-        color: "#999999",
-        fontWeight: "500",
+        color: '#999999',
+        fontWeight: '500',
     },
     noDataButton: {
         width: 250,
         height: 40,
         margin: 20,
-    }
+    },
 });
+
+@observer
+class Currencies extends React.PureComponent {
+    render() {
+        const { navigator, store } = this.props;
+
+        const renderRow = (data, sectionID, rowID) => (
+            <Cell
+                symbol={data.displaySymbol}
+                price={data.price}
+                dailyChangePercent={data.dailyChangePercent}
+                onPress={() => {
+                    store.selectSymbol(data.symbol);
+                    navigator.push({ name: CurrencyDetailsRoute });
+                }}
+                onVote={(sentiment) => {
+                    store.selectSymbol(data.symbol);
+                    store.addSentiment({
+                        asset: data.symbol,
+                        price: data.price,
+                        sentiment,
+                        timestamp: moment().unix(),
+                    });
+                    navigator.push({ name: CurrencyDetailsRoute });
+                }}
+            />
+
+            );
+
+        const renderSeparator = (sectionID, rowID, adjacentRowHighlighted) => (
+            <View key={rowID} style={styles.separator} />
+        );
+
+        const addCurrency = () => {
+            navigator.push({ name: AddCurrencyRoute });
+        };
+
+        const isEmpty = _.isEmpty(store.tickers.slice());
+
+        const noDataView = (
+            <View style={styles.noData}>
+                <Text style={styles.noDataText}>Nothing here.</Text>
+                <Button
+                    raised
+                    buttonStyle={styles.noDataButton}
+                    textStyle={{ textAlign: 'center' }}
+                    title={'Add coins'}
+                    backgroundColor={'#27aa36'}
+                    fontSize={20}
+                    onPress={addCurrency}
+                />
+            </View>
+        );
+
+        const listView = (
+            <ListView
+                style={styles.listView}
+                renderRow={renderRow}
+                renderHeader={() => <View style={styles.header} />}
+                renderFooter={() => <View style={styles.footer} />}
+                renderSeparator={renderSeparator}
+                dataSource={store.dataSource}
+                enableEmptySections
+                removeClippedSubviews={false}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={store.isLoading}
+                        onRefresh={store.refresh}
+                    />
+                }
+            />
+        );
+
+        const contentView = isEmpty ? noDataView : listView;
+
+        return (
+            <View style={styles.container}>
+
+                <NavigationBar
+                    title={{ title: 'Currencies' }}
+                    style={styles.navBar}
+                    rightButton={
+                        <Icon
+                            containerStyle={styles.toolbarButton}
+                            name="mode-edit"
+                            onPress={() => {
+                                navigator.push({ name: EditCurrenciesRoute });
+                            }}
+                        />
+                    }
+                />
+
+                {contentView}
+
+                <View style={styles.fabContainer}>
+
+                    <Icon
+                        raised
+                        reverse
+                        name="add"
+                        color="#27aa36"
+                        onPress={addCurrency}
+                    />
+
+                </View>
+
+            </View>
+        );
+    }
+}
+
+Currencies.propTypes = propTypes;
+
+export default Currencies;
+
