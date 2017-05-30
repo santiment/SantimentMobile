984d301884a6f341adf13044a787762febc32662
Eugene Tartakovsky <flar49@gmail.com>
2017-05-30 15:29:00 +0300

    Lint app/index.js

--- app/index.js
+++ app/index.js
@@ -3,19 +3,18 @@
  * @flow
  */
 
-'use strict';
 
 import React from 'react';
 
-import AppNavigator from './navigator'
-import Store from './store'
+import AppNavigator from './navigator';
+import Store from './store';
 
 class App extends React.Component {
     render() {
         return (
-            <AppNavigator store={Store}/>
-        )
+            <AppNavigator store={Store} />
+        );
     }
 }
 
-export default App
+export default App;
