8ad54b2846240d52ec6636d9841f3c1430be179a
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-06 04:24:18 +0300

    Create Palette class https://github.com/santiment/SantimentMobile/issues/72

---
+++ app/resources/colors/index.js
@@ -0,0 +1,91 @@
+/**
+ * @flow
+ */
+
+/**
+ * Provides base colors.
+ */
+const Palette = {
+    
+    get white(): string {
+        return '#ffffff';
+    },
+
+    get black(): string {
+        return '#000000';
+    },
+
+    get springGreen(): string {
+        return '#00e36f';
+    },
+
+    get silver(): string {
+        return '#cccccc';
+    },
+
+    get amaranthOne(): string {
+        return '#e73c4c';
+    },
+
+    get amaranthTwo(): string {
+        return '#e53e50';
+    },
+
+    get whiteSmoke(): string {
+        return '#f0f0f0';
+    },
+
+    get dustyGray(): string {
+        return '#999999';
+    },
+
+    get forestGreen(): string {
+        return '#27aa36';
+    },
+
+    get mineShaft(): string {
+        return '#333333';
+    },
+
+    get mercury(): string {
+        return '#e6e6e6';
+    },
+
+    get charcoal(): string {
+        return '#454545';
+    },
+
+    get veryLightGray(): string {
+        return '#cdcdcd';
+    },
+
+    get fireBrick(): string {
+        return '#bd2c27';
+    },
+
+    get spunPearl(): string {
+        return '#b1b1b2';
+    },
+
+    get suvaGray(): string {
+        return '#919191';
+    },
+
+    get concrete(): string {
+        return '#f2f2f2';
+    },
+
+    get silverChalice(): string {
+        return '#aaaaaa';
+    },
+
+    get boulder(): string {
+        return '#777777';
+    },
+
+    get mountainMeadow(): string {
+        return '#24e174';
+    },
+};
+
+export default Palette;
