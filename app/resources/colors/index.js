5b549309c7b01486fa2c4e61f218629ccba19abd
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-06 18:03:35 +0300

    Made fields in Palette class static https://github.com/santiment/SantimentMobile/issues/72

--- app/resources/colors/index.js
+++ app/resources/colors/index.js
@@ -5,87 +5,47 @@
 /**
  * Provides base colors.
  */
-const Palette = {
+class Palette {
+
+    static white: string = '#ffffff';
+
+    static black: string = '#000000';
+
+    static springGreen: string = '#00e36f';
+
+    static silver: string = '#cccccc';
     
-    get white(): string {
-        return '#ffffff';
-    },
+    static amaranthOne: string = '#e73c4c';
+    
+    static amaranthTwo: string = '#e53e50';
+    
+    static whiteSmoke: string = '#f0f0f0';
+    
+    static dustyGray: string = '#999999';
+    
+    static forestGreen: string = '#27aa36';
+    
+    static mineShaft: string = '#333333';
+    
+    static mercury: string = '#e6e6e6';
+    
+    static charcoal: string = '#454545';
+    
+    static veryLightGray: string = '#cdcdcd';
+    
+    static fireBrick: string = '#bd2c27';
+    
+    static spunPearl: string = '#b1b1b2';
+    
+    static suvaGray: string = '#919191';
+    
+    static concrete: string = '#f2f2f2';
+    
+    static silverChalice: string = '#aaaaaa';
+    
+    static boulder: string = '#777777';
 
-    get black(): string {
-        return '#000000';
-    },
-
-    get springGreen(): string {
-        return '#00e36f';
-    },
-
-    get silver(): string {
-        return '#cccccc';
-    },
-
-    get amaranthOne(): string {
-        return '#e73c4c';
-    },
-
-    get amaranthTwo(): string {
-        return '#e53e50';
-    },
-
-    get whiteSmoke(): string {
-        return '#f0f0f0';
-    },
-
-    get dustyGray(): string {
-        return '#999999';
-    },
-
-    get forestGreen(): string {
-        return '#27aa36';
-    },
-
-    get mineShaft(): string {
-        return '#333333';
-    },
-
-    get mercury(): string {
-        return '#e6e6e6';
-    },
-
-    get charcoal(): string {
-        return '#454545';
-    },
-
-    get veryLightGray(): string {
-        return '#cdcdcd';
-    },
-
-    get fireBrick(): string {
-        return '#bd2c27';
-    },
-
-    get spunPearl(): string {
-        return '#b1b1b2';
-    },
-
-    get suvaGray(): string {
-        return '#919191';
-    },
-
-    get concrete(): string {
-        return '#f2f2f2';
-    },
-
-    get silverChalice(): string {
-        return '#aaaaaa';
-    },
-
-    get boulder(): string {
-        return '#777777';
-    },
-
-    get mountainMeadow(): string {
-        return '#24e174';
-    },
-};
+    static mountainMeadow: string = '#24e174';
+}
 
 export default Palette;
