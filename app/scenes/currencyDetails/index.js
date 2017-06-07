d867fce5e0799788087b512e0ad94a72dfc4f0ec
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-07 21:29:57 +0300

    Close CurrencyDetails instruction by clicking Android back button

--- app/scenes/currencyDetails/index.js
+++ app/scenes/currencyDetails/index.js
@@ -190,7 +190,19 @@ class CurrencyDetails extends React.Component {
 
                 <AndroidBackButton
                     onPress={() => {
-                        navigator.pop();
+                        /**
+                         * If instruction is visible, close instruction.
+                         * Otherwise, go to previous screen.
+                         */
+                        if (store.isInstructionVisible) {
+                            store.setInstructionVisible(false);
+                        } else {
+                            navigator.pop();
+                        }
+
+                        /**
+                         * Return `true` to prevent closing app.
+                         */
                         return true;
                     }}
                 />
