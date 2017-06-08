b1b09fa3556262e8568d45abc1bf13fc4c515f10
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-08 23:22:58 +0300

    Implement RNConfig class in iOS project https://github.com/santiment/SantimentMobile/issues/74

---
+++ ios/SantimentMobile/RNConfig.h
@@ -0,0 +1,13 @@
+//
+//  RNConfig.h
+//  SantimentMobile
+//
+//  Created by Igor Matyushkin on 08.06.17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNConfig : NSObject
+
+@end
