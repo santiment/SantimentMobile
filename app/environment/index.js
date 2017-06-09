b1d98b6a436c1e385959c0d2bf04e643ca3f0b7d
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-09 17:52:48 +0300

    Added documentation to Environment class

--- app/environment/index.js
+++ app/environment/index.js
@@ -10,8 +10,25 @@ import Configurations from './configurations.json';
 
 const configurationName = RNConfig.buildEnvironment;
 
+/**
+ * Represents current environment.
+ * Provides access to environment parameters.
+ *
+ * All parameters are static, so you can use them
+ * without creating instance of `Environment` class.
+ *
+ * Example of usage:
+ *
+ * ```javascript
+ * // Obtain base URL for Santiment API
+ * const apiUrl = Environment.santimentApiUrl;
+ * ```
+ */
 class Environment {
 
+    /**
+     * Base URL for Santiment API.
+     */
     static get santimentApiUrl(): string {
         return Configurations[configurationName].santimentApiUrl;
     }
