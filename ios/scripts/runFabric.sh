d71dba11a86356fa58bb1a3bc825b29e6343e835
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-06-28 03:46:34 +0300

    Updated build scripts for Xcode project

--- ios/scripts/runFabric.sh
+++ ios/scripts/runFabric.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo "FABRIC_API_KEY = \"${FABRIC_API_KEY}\""
-echo "FABRIC_API_SECRET = \"${FABRIC_API_SECRET}\""
+# Read preprocessor definitions
+eval "${GCC_PREPROCESSOR_DEFINITIONS}"
 
+# Obtain path to environment file
+if [ ${IS_PRODUCTION_BUILD} == 1 ]; then
+    ENVIRONMENT_FILE_PATH="../app/config/production.env"
+elif [ ${IS_STAGING_BUILD} == 1 ]; then
+    ENVIRONMENT_FILE_PATH="../app/config/staging.env"
+else
+    ENVIRONMENT_FILE_PATH=""
+fi
+
+# Read environment file
+. "${ENVIRONMENT_FILE_PATH}"
+
+# Launch Fabric
+echo "Launching Fabric with API key \"${FABRIC_API_KEY}\" and API secret \"${FABRIC_API_SECRET}\""
 "${PODS_ROOT}/Fabric/run" ${FABRIC_API_KEY} ${FABRIC_API_SECRET}
