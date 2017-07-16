08d10fa49bf1f5c59951bcba8706646c41974634
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-07-16 18:19:16 +0300

    Configure app before initial screen appears https://github.com/santiment/SantimentMobile/issues/85

--- app/index.js
+++ app/index.js
@@ -8,10 +8,22 @@ import React from 'react';
 
 import AppNavigator from './navigator';
 import Store from './store';
+import Environment from './config';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.PureComponent {
+    
+    configureApp = (): void => {
+        if (Environment.buildType.isProduction) {
+            console.disableYellowBox = true;
+        } else if (Environment.buildType.isStaging) {
+            console.disableYellowBox = false;
+        }
+    };
+
     render() {
+        this.configureApp();
+
         return (
             <AppNavigator store={Store} />
         );
